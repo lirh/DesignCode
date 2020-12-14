f344a366d59f12189a91e1aa5c812d4ee76d34a8
LiRH <li.rh@icloud.com>
2020-12-14 22:08:56 +0800

    lirh
    底部card的拖拽效果

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
     @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
     
     var body: some View {
         ZStack {
@@ -93,10 +95,35 @@ struct ContentView: View {
                 )
             
             BottomCardView()
-                .offset(x: 0.0, y: showCard ? 80 : 300)
-                .offset(x: 0.0, y: show ? 3000 : 300)
+                .offset(x: 0.0, y: showCard ? 360 : 1000)
+                .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
                 .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                .gesture(
+                    DragGesture()
+                        .onChanged{ value in
+                            bottomState = value.translation
+                            if showFull {
+                                bottomState.height += -300
+                            }
+                            if bottomState.height < -300 {
+                                bottomState.height = -300
+                            }
+                        }
+                        .onEnded{ value in
+                            if bottomState.height > 50 {
+                                showCard = false
+                            }
+                            if (bottomState.height < -100 && !showFull) || (bottomState.height < -250 && showFull){
+                                bottomState.height = -300
+                                showFull = true
+                            } else {
+                                bottomState = CGSize.zero
+                                showFull = false
+                            }
+                          
+                        }
+                    )
                 
         }
     }
@@ -106,8 +133,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
-            ContentView()
-                .preferredColorScheme(.dark)
+//            ContentView()
+//                .preferredColorScheme(.dark)
         }
     }
 }
