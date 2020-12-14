5191e61c54a31f4db1c3b5db550251816d4cd30b
LiRH <li.rh@icloud.com>
2020-12-14 22:50:06 +0800

    lirh
    .overlay 切割圆形头像
    Color利用多层frame和背景色制作进度条

--- DesignCode/MenuView.swift
+++ DesignCode/MenuView.swift
@@ -13,6 +13,20 @@ struct MenuView: View {
             Spacer()
             
             VStack(spacing: 16) {
+                Text("Meng - 28% complate")
+                    .font(.caption)
+                
+                //利用多层frame和背景色制作进度条
+                Color.white
+                    .frame(width: 30, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .padding()
+                    .frame(width: 150, height: 24, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
@@ -21,12 +35,21 @@ struct MenuView: View {
             .frame(maxWidth: .infinity)
             //高度
             .frame(height: 300)
-            .background(Color.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
             //圆角矩形切割
             .clipShape(RoundedRectangle(cornerRadius: 30, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             //内边距
             .padding(.horizontal,30)
+            //切割圆形头像
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
         }
         //底边距
         .padding(.bottom,30)
@@ -50,6 +73,7 @@ struct MenuRow: View {
                 //图像比例 大、中、小
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.662745098, green: 0.7333333333, blue: 0.831372549, alpha: 1)))
          
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .default))
