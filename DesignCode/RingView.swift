9d6a1a5f6296fa61651e08c092ed536beae32c76
LiRH <li.rh@icloud.com>
2021-01-03 21:34:34 +0800

    lirh
    RingView 组建 根据变量变化

--- DesignCode/RingView.swift
+++ DesignCode/RingView.swift
@@ -8,19 +8,26 @@
 import SwiftUI
 
 struct RingView: View {
+    var color1 = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
+    var color2 = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+    var width: CGFloat = 88
+    var height: CGFloat = 88
+    var percent: CGFloat = 88
+    
     var body: some View {
-        
+        let multiplier = width / 44
+        let progress = 1 - (percent / 100)
         
         ZStack {
             Circle()
-                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5))
-                .frame(width: 44, height: 44)
+                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5 * multiplier))
+                .frame(width: width, height: height)
             
             Circle()
-                .trim(from: 0.2, to: 1)
+                .trim(from: progress, to: 1)
                 .stroke(
-                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .leading, endPoint: .trailing),
-                    style: StrokeStyle(lineWidth: 5,
+                    LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .leading, endPoint: .trailing),
+                    style: StrokeStyle(lineWidth: 5 * multiplier,
                                        lineCap: .round,
                                        lineJoin: .round,
                                        miterLimit: .infinity,
@@ -32,11 +39,11 @@ struct RingView: View {
                     Angle(degrees: 180),
                     axis: (x: 1.0, y: 0, z: 0.0)
                 )
-                .frame(width: 44, height: 44)
-                .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), radius: 3, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 3)
+                .frame(width: width, height: height)
+                .shadow(color: Color(color2), radius: 3 * multiplier, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 3 * multiplier)
             
-            Text("82%")
-                .font(.subheadline)
+            Text("\(Int(percent))%")
+                .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
         }
     }
@@ -44,6 +51,6 @@ struct RingView: View {
 
 struct RingView_Previews: PreviewProvider {
     static var previews: some View {
-        RingView()
+        RingView(width: 88, height: 88, percent: 22)
     }
 }
